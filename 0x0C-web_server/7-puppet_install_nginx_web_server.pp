77135b3532dc7ae980eb93655ecc1f26da65c289
Mally13 <mallymutuku13@gmail.com>
2023-11-30 12:10:13 +0300

    Task 5. Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,65 +1,24 @@
-# puppet conf to Install Nginx with the following configurations:
-#+	Listens on port 80.
-#+	Returns a page containing "Holberton School" when queried
-#+	 at the root with a curl GET request.
-# Configures /redirect_me as a "301 Moved Permanently".
-# Includes a custom 404 page containing "Ceci n'est pas une page".
+# Setup New Ubuntu server with nginx
 
-node 'default' {
-  # Install Nginx package
-  package {
-	'nginx':
-	  ensure => present,
-	}
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
 
-  # Create /etc/nginx/sites-available/default directory
-  file {
-	'/etc/nginx/sites-available/default':
-	  ensure => directory,
-	  mode   => '0755',
-	  owner  => 'root',
-	  group  => 'root',
-	}
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
 
-  # Create /etc/nginx/sites-enabled/default symlink
-  file {
-	'/etc/nginx/sites-enabled/default':
-	  ensure => link,
-	  target  => '/etc/nginx/sites-available/default',
-	}
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
 
-  # Configure Nginx server block
-  file {
-	'/etc/nginx/sites-available/default':
-	  ensure => present,
-	  content => <<'EOF'
-	  server {
-	    listen 80;
-	    listen [::]:80 default_server;
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
 
-	    root /var/www/html;
-	    index index.html index.htm;
-
-	    location / {
-	      return 200 "Hello World!";
-	    }
-
-	    location /redirect_me {
-	      return 301 https://www.example.com;
-	    }
-
-	    error_page 404 /404.html;
-	    location /404 {
-	      return 404 "Ceci n'\''est pas une page\n";
-	    }
-	  }
-	EOF
-  }
-
-  # Restart Nginx service
-  service {
-	'nginx':
-	  ensure => running,
-	  enable => true,
-	}
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }

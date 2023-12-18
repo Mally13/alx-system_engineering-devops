5a48a3da56ef4db7adc3994e59ec46aa4710da4a
Mally13 <mallymutuku13@gmail.com>
2023-12-18 17:02:04 +0300

    Task 2: Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,67 +1,8 @@
-# Define variables
-$directory = '/etc/nginx/html'
-$file_404 = '/etc/nginx/html/404.html'
-$file_index = '/etc/nginx/html/index.html'
-
-# Update package lists
-package { 'nginx':
-  ensure => 'latest',
+# Automation: creates a custom HTTP header response with Puppet.
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
-
-# Ensure nginx service is running
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Package['nginx'],
-}
-
-# Print hostname
-notify { $::hostname: }
-
-# Create directory if it doesn't exist
-file { $directory:
-  ensure => 'directory',
-}
-
-# Create index.html file if it doesn't exist
-file { $file_index:
-  ensure  => 'file',
-  content => 'Hello World!',
-}
-
-# Create 404.html file if it doesn't exist
-file { $file_404:
-  ensure  => 'file',
-  content => "Ceci n'est pas une page",
-}
-
-# Configure nginx default site
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  content => "server {
-	listen 80;
-	listen [::]:80 default_server;
-	root   /etc/nginx/html;
-	index  index.html index.htm;
-	add_header X-Served-By ${hostname} always;
-
-	location /redirect_me {
-	    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-	}
-	error_page 404 /404.html;
-	location /404 {
-	  root /etc/nginx/html;
-	  internal;
-	}
-}",
-  require => [Package['nginx'], File[$directory], File[$file_index], File[$file_404]],
-}
-
-# Restart nginx service
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => File['/etc/nginx/sites-available/default'],
-  subscribe => [Package['nginx'], File[$directory], File[$file_index], File[$file_404]],
-}
-

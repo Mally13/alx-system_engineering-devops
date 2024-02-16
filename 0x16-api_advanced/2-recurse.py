3c2ada7740cd18142120b994032bcbefe93df168
Mally13 <mallymutuku13@gmail.com>
2024-02-16 17:06:22 +0300

    Task 2: recursion

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit"""
+"""
+Queries the Reddit API and returns a list containing the titles
+of all hot articles for a given subreddit
+"""
 import requests
 
 

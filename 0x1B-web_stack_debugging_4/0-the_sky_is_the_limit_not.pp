0d271061bd25fe381058f362c2a4a70b0750ca4a
Mally13 <mallymutuku13@gmail.com>
2024-02-25 17:36:28 +0300

    Task 0: Increase ulimit of the nginx default file

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,15 @@
+#Fixes too many open files error when there
+#are many requests
+
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path	=> '/usr/local/bin/:/bin'
+}
+
+#restart Nginx
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path	=> '/etc/init.d/'
+}
